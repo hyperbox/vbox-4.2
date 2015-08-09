e4bc568765f85bfbec3e3c47fc8c554b001f1ace
Maxime Dor <max@kamax.io>
2015-08-09 14:48:05 +0200

    Refactored some common code between versions out

--- src/server/xpcom/src/io/kamax/vbox4_2/VBoxXpcomHypervisor.java
+++ src/server/xpcom/src/io/kamax/vbox4_2/VBoxXpcomHypervisor.java
@@ -23,10 +23,8 @@ package io.kamax.vbox4_2;
 import io.kamax.hbox.exception.HypervisorException;
 import io.kamax.hboxd.hypervisor.Hypervisor;
 import io.kamax.tool.AxStrings;
-import io.kamax.tool.logging.Logger;
+import io.kamax.vbox.VBoxXPCOM;
 import io.kamax.vbox.VirtualBox;
-import io.kamax.vbox4_2.VBoxHypervisor;
-import java.io.File;
 import org.virtualbox_4_2.ISession;
 import org.virtualbox_4_2.VirtualBoxManager;
 
@@ -37,8 +35,6 @@ import org.virtualbox_4_2.VirtualBoxManager;
       product = VirtualBox.PRODUCT,
       schemes = { VirtualBox.ID.XPCOM_4_2 })
 public final class VBoxXpcomHypervisor extends VBoxHypervisor {
-
-   private final String defaultHome = "/usr/lib/virtualbox";
 
    @Override
    public String getId() {
@@ -53,15 +49,10 @@ public final class VBoxXpcomHypervisor extends VBoxHypervisor {
    @Override
    protected VirtualBoxManager connect(String options) {
       if (AxStrings.isEmpty(options)) {
-         options = defaultHome;
+         options = VBoxXPCOM.getDefaultHome();
       }
 
-      Logger.debug("Options - " + options);
-      Logger.debug("vbox.home - " + System.getProperty("vbox.home"));
-      Logger.debug("java.library.path - " + System.getProperty("java.library.path"));
-      File libxpcom = new File(options + "/libvboxjxpcom.so");
-      Logger.debug("Lib exists - " + libxpcom.getAbsolutePath() + " - " + libxpcom.isFile());
-
+      VBoxXPCOM.triggerVBoxSVC(options);
       VirtualBoxManager mgr = VirtualBoxManager.createInstance(options);
       if (mgr.getVBox().getVersion().contains("OSE") && (mgr.getVBox().getRevision() < 50393)) {
          throw new HypervisorException(
